e054d50909084ac8dd1e62f5d5b8d920880e988e
MiyukiIto3 <rd009074@gmail.com>
2022-04-27 13:19:06 +0900

    add: top画面から投稿作成画面までの遷移テストを作成

--- spec/features/homes_features_spec.rb
+++ spec/features/homes_features_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.feature "Tops", type: :feature do
-  describe "トップ画面" do
+  describe "header" do
     context "ログインしてない場合" do
       before do
         visit root_path
@@ -54,6 +54,57 @@ RSpec.feature "Tops", type: :feature do
         click_link "Logout"
         expect(current_path).to eq root_path
       end
+    end
+  end
+
+  describe "top" do
+    let(:user) { create :user }
+
+    before do
+      sign_in user
+      visit root_path
+    end
+
+    it "はじめるボタン リンク先が正しいこと" do
+      click_on "はじめる"
+      expect(current_path).to eq homes_select_kid_path
+    end
+  end
+
+  describe "select_kid" do
+    let(:user) { create :user }
+    let(:kid) { create :kid, user: user }
+    let!(:user_kids) { create_list(:kid, 2, user: user) }
+
+    before do
+      sign_in user
+      visit homes_select_kid_path
+    end
+
+    it "子供の名前ボタン リンク先が正しいこと" do
+      first("button").click
+      expect(current_path).to eq homes_select_type_path
+    end
+  end
+
+  describe "select_kid" do
+    let(:user) { create :user }
+    let(:kid) { create :kid, user: user }
+    let!(:post_params) { attributes_for(:post) }
+
+    before do
+      sign_in user
+      visit homes_select_type_path params: { post: post_params }
+    end
+
+    it "お金をもらったボタン のリンク先が正しいこと" do
+      click_on "お金をもらった"
+      expect(current_path).to eq new_post_path
+    end
+
+    it "お金をつかったボタン のリンク先が正しいこと" do
+      click_on "お金をつかった"
+      expect(current_path).to eq new_post_path
     end
   end
 end
