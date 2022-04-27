a67aa6a54871b7e2bca97901b2ca72a7d8ec3e12
MiyukiIto3 <rd009074@gmail.com>
2022-04-27 14:24:36 +0900

    herokuエラー対応

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_21_012622) do
+ActiveRecord::Schema.define(version: 2022_04_21_005855) do
 
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
